85aad20db169f6b154f13f5e55e7a98ae8145ec7
Phyllis Kan <pyk9q7@gmail.com>
2019-08-09 05:32:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,11 +10,8 @@ def map(array)
 end
 
 def reduce(array, starting_point=0)
-  i = 0
-  sum = starting_point
-  while i < array.length do
-    sum = sum(yield + (array[i]))
-    i += 1
+  array.length.times do |i|
+    starting_point = yield(starting_point, array[i])
   end
-  return sum
+  starting_point
 end
